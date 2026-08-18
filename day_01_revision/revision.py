ddc9aae08edd7bf62819e4fe3375d08f054fbb38
Arun Kumar <arun.cyberx@gmail.com>
2026-08-18 22:51:56 +0530

    Refactor README and revision.py to consolidate code examples for clarity

--- day_01_revision/revision.py
+++ day_01_revision/revision.py
@@ -4,3 +4,28 @@
 
 
 
+# 2. Strings
+
+# code are in readme.md
+
+
+# 3. Lists
+
+marks = [85, 72, 91, 66, 88]
+
+print(len(marks))
+print(sum(marks))
+print((max(marks)))
+print(min(marks))
+
+marks.append(95)
+print(marks)
+
+marks.remove(66)
+print(marks)
+
+marks.sort()
+print(marks)
+
+print(sorted(marks))
+
